73a54427904b240b3b9fd6dc92ad087b49634d68
GuntisWidget <147302670+GuntisWidget@users.noreply.github.com>
2023-10-08 10:32:07 +0300

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    HOST: "dpg-ckh5j2eafg7c7381q73g-a",
+    USER: "grady",
+    PORT: 5432,
+    PASSWORD: "VcMCMBK2Dab8Jmo0g7QHX1ExExwfVj4r",
+    DB: "guntisdb",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+//
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
